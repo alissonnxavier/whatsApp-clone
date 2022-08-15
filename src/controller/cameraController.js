7d3ed3a0a6a75852656501079c1b05ce81669518
Xavier <alissonalves29@gmail.com>
2022-08-14 21:48:46 -0300

    adicionado funçao para tirar foto

--- src/controller/cameraController.js
+++ src/controller/cameraController.js
@@ -16,7 +16,7 @@ export class CameraController{
 
             console.log(err);
         })
-    }
+    };
 
     stop(){
 
@@ -24,5 +24,22 @@ export class CameraController{
 
             track.stop();
         })
+    };
+
+    takePicture(mimeType = 'image/png'){
+
+        let canvas = document.createElement('canvas');
+
+        canvas.setAttribute('height', this._videoEl.videoHeight);
+        canvas.setAttribute('width', this._videoEl.videoWidth);
+
+        let context = canvas.getContext('2d');
+
+        context.drawImage(this._videoEl, 0, 0, canvas.width, canvas.height);
+
+        return canvas.toDataURL(mimeType);
     }
+
+
+
 }
